77ec720460e626537629a16ad41ab6599d25e1fa
Javid <sairexm@gmail.com>
2024-06-17 20:01:28 +0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //TASK 1
+        int[] ThreeIntArray = new int[]{1, 2, 3};
+        double[] ThreeFractionsArray = {1.57, 7.654, 9.986};
+        String[] ThreeStringArray = {"One", "Two", "Three"};
+    }
+}
